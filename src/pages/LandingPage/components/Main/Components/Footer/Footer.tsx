4abd76aee2f2c9381045372097d5b6b6cdb7cdcf
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-07-18 18:20:15 -0300

    add footer

---
+++ src/pages/LandingPage/components/Main/Components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import styles from '../../../../../../styles/footer.module.scss'
+
+export function Footer() {
+    return (
+        <footer className={styles.Container} >
+            <div className={styles.Contacts_Wrap} >
+                <a>facebook</a>
+                <a>instagram</a>
+                <a>Email</a>
+                <a>twitter</a>
+            </div>
+            <span>clone de <a href="http://momentum-template.webflow.io/" >template</a> feito por <a href="https://github.com/lucaslpa12345" > Lucas Pereira </a> </span>
+        </footer>
+    )
+}
