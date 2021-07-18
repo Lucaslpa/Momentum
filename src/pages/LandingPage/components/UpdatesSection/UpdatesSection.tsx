c180f1abbcef9e75ade86a3d29d561fdac585c74
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-07-17 23:09:49 -0300

    add UpdateSection component

---
+++ src/pages/LandingPage/components/UpdatesSection/UpdatesSection.tsx
@@ -0,0 +1,16 @@
+import styles from '../../../../styles/update.module.scss'
+export function UpdateSction() {
+
+    return (
+        <div className={styles.Container} >
+            <div className={styles.Wrapper} >
+               <h1>Get updates</h1>
+               <p>Lorem ipsum dolor sit.</p>
+               <form>
+                   <input placeholder='Enter your email' ></input>
+                   <button>Subscribe</button>
+              </form>
+            </div>
+        </div>
+    )
+}
