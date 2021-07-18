650e6703873af4bc9fa9a98bcbafcdc29bf749c7
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-07-17 23:10:55 -0300

    landing page mod

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,18 +1,26 @@
-import Head from 'next/head'
+/* eslint-disable @next/next/no-img-element */
 import styles from '../../styles/main.module.scss'
 import {Header} from './components/Header/Header'
+import {HomeSection} from './components/HomeSection/HomeSection'
+import {AboutSection} from './components/AboutSection/AboutSection'
+import {FeatureSection} from './components/FeatureSection/FeatureSection'
+import {DownloadSection} from './components/DownloadSection/DownloadSection'
+import {UpdateSction} from './components/UpdatesSection/UpdatesSection'
+import {Footer} from './components/Footer/Footer'
 
 export  function LandingPage() {
   return (
-    <div className={styles.Container}>
-      <Head>
-        <title>Landing Page</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+  <div className={styles.Container}>
    <Header/>
-
- 
-   
-    </div>
+    <main>
+      <HomeSection/>
+      <AboutSection/>
+      <FeatureSection/>
+      <img className={styles.bottom_IMG} alt=''  src="https://uploads-ssl.webflow.com/5af49de0dbb1e3637f2d8886/5b10cd0c3a8396da6772516b_bg-apps.jpg" />
+      <DownloadSection/>
+      <UpdateSction/>
+    </main>
+    <Footer/>
+  </div>
   )
 }
