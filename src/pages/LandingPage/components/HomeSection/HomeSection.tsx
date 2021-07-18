d8b1464963edb8e216014baf5af06f00ab507596
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-07-17 21:25:57 -0300

    Add HomeSection Component

---
+++ src/pages/LandingPage/components/HomeSection/HomeSection.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import decorationIMG from '../../../../public/back.png'
+import styles from '../../../../styles/sectionhome.module.scss'
+
+
+
+export function HomeSection() {
+
+
+    return (
+        <div className={styles.Container} > 
+         
+             <div className={styles.Interactions} >
+                   <h1 className={styles.Text} >Momentum</h1>
+                   <h3 className={styles.Sub_Text} >Launch the product-and savor the momemtum</h3>
+                   <button className={styles.Button } >Get the App</button>
+             </div>
+             <div className={styles.decoration_IMG} >
+                <Image alt= '' src={decorationIMG} />
+            </div>
+             <div>
+                 <Image alt='' src='https://uploads-ssl.webflow.com/5af49de0dbb1e3637f2d8886/5b1437cbda3c5c3369fe4460_arrow.svg' 
+                 width={50}
+                 height={50}
+                 />
+             </div>
+        </div>
+    )
+}
